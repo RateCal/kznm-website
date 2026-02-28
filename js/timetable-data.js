5b34a8322e71d8c06d9bd72897c6ac3466cc193c
ああるらて <syokutorinkai@gmail.com>
2026-02-28 13:35:42 +0900

    時刻表を更新 [admin 2026/2/28 13:35:41]

---
+++ js/timetable-data.js
@@ -0,0 +1,30 @@
+// js/timetable-data.js
+// eるりきゅう 時刻表管理システム 出力 2026/2/28 13:35:41
+// ※ 手動編集より管理システム (admin.html) の使用を推奨します。
+
+const TIMETABLE = {
+  "trains": [
+    {
+      "id": "train-1772197348775",
+      "type": "rapid",
+      "name": "田所 810号",
+      "runDays": [
+        "2026-02-27"
+      ],
+      "stops": [
+        {
+          "code": "A1",
+          "arr": null,
+          "dep": "08:10"
+        },
+        {
+          "code": "C1",
+          "arr": "19:19",
+          "dep": null
+        }
+      ],
+      "notice": "下北沢へは参りませんので、ご注意ください。",
+      "trainNo": "9810A"
+    }
+  ]
+};
