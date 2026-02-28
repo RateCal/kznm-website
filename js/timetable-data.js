a0a8fa530678f5ab5ebd89b178322d43b40e6d7e
ああるらて <syokutorinkai@gmail.com>
2026-02-28 16:38:41 +0900

    時刻表を更新 [admin 2026/2/28 16:38:41]

--- js/timetable-data.js
+++ js/timetable-data.js
@@ -1,12 +1,12 @@
 // js/timetable-data.js
-// eるりきゅう 時刻表管理システム 出力 2026/2/28 13:43:34
+// eるりきゅう 時刻表管理システム 出力 2026/2/28 16:38:40
 // ※ 手動編集より管理システム (admin.html) の使用を推奨します。
 
 const TIMETABLE = {
   "trains": [
     {
       "id": "train-1772197348775",
-      "type": "rapid",
+      "type": "special-rapid",
       "name": "田所 810号",
       "runDays": [
         "2026-02-27",
@@ -50,6 +50,29 @@ const TIMETABLE = {
           "seatClass": "reserved",
           "seats": 16,
           "note": "車内は禁煙です。喫煙室をご利用ください。"
+        }
+      ],
+      "reserveOpen": true
+    },
+    {
+      "id": "train-1772262134376",
+      "type": "charter",
+      "trainNo": "9951",
+      "name": "修学旅行",
+      "runDays": [],
+      "notice": "",
+      "charterPass": "0050",
+      "cars": [],
+      "stops": [
+        {
+          "code": "X01",
+          "arr": null,
+          "dep": "08:45"
+        },
+        {
+          "code": "Y02",
+          "arr": "09:13",
+          "dep": null
         }
       ]
     }
