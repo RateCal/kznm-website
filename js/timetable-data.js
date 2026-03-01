8b65616337d6e52d4ff860c897634f27507b6084
ああるらて <syokutorinkai@gmail.com>
2026-03-01 17:11:09 +0900

    サイト一括更新 (17ファイル) [admin 2026/3/1 17:11:09]

--- js/timetable-data.js
+++ js/timetable-data.js
@@ -1,5 +1,5 @@
 // js/timetable-data.js
-// eるりきゅう 時刻表管理システム 出力 2026/3/1 17:10:53
+// eるりきゅう 時刻表管理システム 出力 2026/3/1 17:10:58
 // ※ 手動編集より管理システム (admin.html) の使用を推奨します。
 
 const TIMETABLE = {
@@ -85,7 +85,7 @@ const TIMETABLE = {
         },
         {
           "carNo": 4,
-          "seatClass": "reserved",
+          "seatClass": "special",
           "seats": 16,
           "note": ""
         }
